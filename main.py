9132cf0c014b8a2452c2144193a1ad7bc3590eda
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2024-04-03 18:39:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
 from flask import Flask, render_template
-
 import datetime
 import os
 x = datetime.datetime.now()
-
-
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html",year=x.year)
-
-
-
 
 if __name__=='__main__':
     app.run(debug=True)
